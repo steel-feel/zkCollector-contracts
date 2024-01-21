fb21514fe73f5a2610c8097838b00b2a9ac70720
himank jain <himankjain@yahoo.in>
2024-01-21 18:49:56 +0530

    actions instead of events

--- src/game.test.ts
+++ src/game.test.ts
@@ -32,20 +32,45 @@ describe('checkpoint 1', () => {
     await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
   })
 
-  it('user should be able prove aasure discovery', async () => {
+  test('user should be able prove treasure discovery', async () => {
     const { privateKey: gamerPrivKey, publicKey: gamerAccount } = Local.testAccounts[0];
     let keyWitness = Tree.getWitness(Field.from(1))
 
     const txn1 = await Mina.transaction(gamerAccount, async () => {
-      await zkAppInstance.foundTreasure(Field.from(2000), Field.from(3000), keyWitness)
+      zkAppInstance.foundTreasure(Field.from(2000), Field.from(3000), keyWitness)
     });
 
     await txn1.prove();
 
     return expect(await txn1.sign([gamerPrivKey]).send()).resolves
 
+  });
 
+
+  test('Previously found treasure should NOT be re-discovered', async () => {
+    const { publicKey: gamerAccount } = Local.testAccounts[0];
+    let keyWitness = Tree.getWitness(Field.from(1))
+
+    return expect(Mina.transaction(gamerAccount, () => {
+      zkAppInstance.foundTreasure(Field.from(2000), Field.from(3000), keyWitness)
+    })).rejects.toThrow()
 
   });
+
+
+  test('Should emit actions', async () => {
+    const { publicKey: gamerAccount } = Local.testAccounts[0];
+
+    const actions = await zkAppInstance.reducer.fetchActions({
+      fromActionState: Reducer.initialActionState
+    });
+    
+    expect(actions[0][0].user.toBase58()).toEqual(gamerAccount.toBase58());
+
+  })
+
+
+
+
 });
 
--- src/game.ts
+++ src/game.ts
@@ -5,30 +5,27 @@ import {
     State,
     state,
     PublicKey,
-    Mina,
     method,
-    UInt32,
-    PrivateKey,
-    AccountUpdate,
-    MerkleTree,
-    MerkleWitness,
     Struct,
     MerkleMapWitness,
+    Reducer,
+    Bool,
 } from 'o1js';
 
 // we need the initiate tree root in order to tell the contract about our off-chain storage
 import { initialCommitment } from "./constants.js"
 
 
-class Point extends Struct({ user:PublicKey, x: Field, y: Field }){}
+class UserTreasure extends Struct({ user: PublicKey, x: Field, y: Field }) { }
 
 export class Game extends SmartContract {
     // a commitment is a cryptographic primitive that allows us to commit to data, with the ability to "reveal" it later
     @state(Field) commitment = State<Field>();
 
-    events = {
-        "user-found-treasure": Point
-    }
+    //Map for storing user enrollment
+    reducer = Reducer({
+        actionType: UserTreasure,
+    });
 
     init() {
         super.init();
@@ -42,13 +39,42 @@ export class Game extends SmartContract {
         const cordinateHash = Poseidon.hash([x, y])
 
         // we check that the cordinates is within the committed Merkle Tree
-
-        // path.calculateRoot(cordinateHash).assertEquals(commitment);
-
         const [rootBefore,] = path.computeRootAndKey(cordinateHash)
         this.commitment.requireEquals(rootBefore);
 
-        this.emitEvent("user-found-treasure", {x,y,user:this.sender});
+        //check if previously commited
+        // past actions 
+        let pendingActions = this.reducer.getActions({ fromActionState: Reducer.initialActionState })
+
+        // initial state of reducer
+        let initial = {
+            state: Bool(false),
+            actionState: Reducer.initialActionState,
+        };
+
+        // checking if the user already exists within the actions
+        let { state: exists } = this.reducer.reduce(
+            pendingActions,
+            Bool,
+            (state: Bool, action: UserTreasure) => {
+
+                const cond1 = action.user.equals(this.sender)
+                    .and(action.x.equals(x))
+                    .and(action.y.equals(y));
+
+                return cond1.or(state);
+            },
+            // initial state
+            initial
+        );
+
+        exists.assertFalse()
+
+        const toEmit = new UserTreasure({
+            x,y,user:this.sender
+        })
+
+        this.reducer.dispatch(toEmit);
     }
 
 }
